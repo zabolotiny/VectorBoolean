8f656728857dbb00ffacec2867b32dab480400d7
Edon <edon@valdman.works>
2023-02-25 14:07:17 -0500

    Update FBEdgeCrossing.swift
    Fixed indentation

--- Sources/VectorBoolean/FBEdgeCrossing.swift
+++ Sources/VectorBoolean/FBEdgeCrossing.swift
@@ -17,180 +17,180 @@ import UIKit
 /// The main piece of data is the intersection, but it also holds a pointer to the
 /// crossing's counterpart in the other FBBezierGraph
 class FBEdgeCrossing {
-
-  fileprivate var _intersection: FBBezierIntersection
-
-  var edge: FBBezierCurve?
-  var counterpart: FBEdgeCrossing?
-  var fromCrossingOverlap = false
-  var entry = false
-  var processed = false
-  var selfCrossing = false
-  var index: Int = 0
-
-  //+ (id) crossingWithIntersection:(FBBezierIntersection *)intersection
-  init(intersection: FBBezierIntersection) {
-    _intersection = intersection
-  }
-
-  var isProcessed : Bool {
-    return processed
-  }
-
-  var isSelfCrossing : Bool {
-    return selfCrossing
-  }
-
-  var isEntry : Bool {
-    return entry
-  }
-
-  //@synthesize edge=_edge;
-  //@synthesize counterpart=_counterpart;
-  //@synthesize entry=_entry;
-  //@synthesize processed=_processed;
-  //@synthesize selfCrossing=_selfCrossing;
-  //@synthesize index=_index;
-  //@synthesize fromCrossingOverlap=_fromCrossingOverlap;
-
-  //@property (assign) FBBezierCurve *edge;
-  //@property (assign) FBEdgeCrossing *counterpart;
-  //@property (readonly) CGFloat order;
-  //@property (getter = isEntry) BOOL entry;
-  //@property (getter = isProcessed) BOOL processed;
-  //@property (getter = isSelfCrossing) BOOL selfCrossing;
-  //@property BOOL fromCrossingOverlap;
-  //@property NSUInteger index;
-
-  // An easy way to iterate crossings. It doesn't wrap when it reaches the end.
-  //@property (readonly) FBEdgeCrossing *next;
-  //@property (readonly) FBEdgeCrossing *previous;
-  //@property (readonly) FBEdgeCrossing *nextNonself;
-  //@property (readonly) FBEdgeCrossing *previousNonself;
-
-  // These properties pass through to the underlying intersection
-  //@property (readonly) CGFloat parameter;
-  ///@property (readonly) FBBezierCurve *curve;
-  //@property (readonly) FBBezierCurve *leftCurve;
-  //@property (readonly) FBBezierCurve *rightCurve;
-  //@property (readonly, getter = isAtStart) BOOL atStart;
-  //@property (readonly, getter = isAtEnd) BOOL atEnd;
-  //@property (readonly) NSPoint location;
-
-
-  //- (void) removeFromEdge
-  func removeFromEdge() {
-    if let edge = edge {
-      edge.removeCrossing(self)
+    
+    fileprivate var _intersection: FBBezierIntersection
+    
+    var edge: FBBezierCurve?
+    var counterpart: FBEdgeCrossing?
+    var fromCrossingOverlap = false
+    var entry = false
+    var processed = false
+    var selfCrossing = false
+    var index: Int = 0
+    
+    //+ (id) crossingWithIntersection:(FBBezierIntersection *)intersection
+    init(intersection: FBBezierIntersection) {
+        _intersection = intersection
     }
-  }
-
-  //- (CGFloat) order
-  var order : Double {
-    return parameter
-  }
-
-  //- (FBEdgeCrossing *) next
-  var next : FBEdgeCrossing? {
-    if let edge = edge {
-      return edge.nextCrossing(self)
-    } else {
-      return nil
+    
+    var isProcessed : Bool {
+        return processed
     }
-  }
-
-  //- (FBEdgeCrossing *) previous
-  var previous : FBEdgeCrossing? {
-    if let edge = edge {
-      return edge.previousCrossing(self)
-    } else {
-      return nil
+    
+    var isSelfCrossing : Bool {
+        return selfCrossing
     }
-  }
-
-  //- (FBEdgeCrossing *) nextNonself
-  var nextNonself : FBEdgeCrossing? {
-    var nextNon : FBEdgeCrossing? = next
-    while nextNon != nil && nextNon!.isSelfCrossing {
-      nextNon = nextNon!.next
+    
+    var isEntry : Bool {
+        return entry
     }
-    return nextNon
-  }
-
-  //- (FBEdgeCrossing *) previousNonself
-  var previousNonself : FBEdgeCrossing? {
-    var prevNon : FBEdgeCrossing? = previous
-    while prevNon != nil && prevNon!.isSelfCrossing {
-      prevNon = prevNon!.previous
+    
+    //@synthesize edge=_edge;
+    //@synthesize counterpart=_counterpart;
+    //@synthesize entry=_entry;
+    //@synthesize processed=_processed;
+    //@synthesize selfCrossing=_selfCrossing;
+    //@synthesize index=_index;
+    //@synthesize fromCrossingOverlap=_fromCrossingOverlap;
+    
+    //@property (assign) FBBezierCurve *edge;
+    //@property (assign) FBEdgeCrossing *counterpart;
+    //@property (readonly) CGFloat order;
+    //@property (getter = isEntry) BOOL entry;
+    //@property (getter = isProcessed) BOOL processed;
+    //@property (getter = isSelfCrossing) BOOL selfCrossing;
+    //@property BOOL fromCrossingOverlap;
+    //@property NSUInteger index;
+    
+    // An easy way to iterate crossings. It doesn't wrap when it reaches the end.
+    //@property (readonly) FBEdgeCrossing *next;
+    //@property (readonly) FBEdgeCrossing *previous;
+    //@property (readonly) FBEdgeCrossing *nextNonself;
+    //@property (readonly) FBEdgeCrossing *previousNonself;
+    
+    // These properties pass through to the underlying intersection
+    //@property (readonly) CGFloat parameter;
+    ///@property (readonly) FBBezierCurve *curve;
+    //@property (readonly) FBBezierCurve *leftCurve;
+    //@property (readonly) FBBezierCurve *rightCurve;
+    //@property (readonly, getter = isAtStart) BOOL atStart;
+    //@property (readonly, getter = isAtEnd) BOOL atEnd;
+    //@property (readonly) NSPoint location;
+    
+    
+    //- (void) removeFromEdge
+    func removeFromEdge() {
+        if let edge = edge {
+            edge.removeCrossing(self)
+        }
     }
-    return prevNon
-  }
-
-  // MARK: Underlying Intersection Access
-  // These properties pass through to the underlying intersection
-
-  //- (CGFloat) parameter
-  var parameter : Double {
-    // TODO: Is this actually working? Check equality operator here!
-    if edge == _intersection.curve1 {
-      return _intersection.parameter1
-    } else {
-      return _intersection.parameter2
+    
+    //- (CGFloat) order
+    var order : Double {
+        return parameter
     }
-  }
-
-  //- (NSPoint) location
-  var location : CGPoint {
-    return _intersection.location
-  }
-
-  //- (FBBezierCurve *) curve
-  var curve : FBBezierCurve? {
-    return edge
-  }
-
-  //- (FBBezierCurve *) leftCurve
-  var leftCurve : FBBezierCurve? {
-    if isAtStart {
-      return nil
+    
+    //- (FBEdgeCrossing *) next
+    var next : FBEdgeCrossing? {
+        if let edge = edge {
+            return edge.nextCrossing(self)
+        } else {
+            return nil
+        }
     }
-
-    if edge == _intersection.curve1 {
-      return _intersection.curve1LeftBezier
-    } else {
-      return _intersection.curve2LeftBezier
+    
+    //- (FBEdgeCrossing *) previous
+    var previous : FBEdgeCrossing? {
+        if let edge = edge {
+            return edge.previousCrossing(self)
+        } else {
+            return nil
+        }
     }
-  }
-
-  //- (FBBezierCurve *) rightCurve
-  var rightCurve : FBBezierCurve? {
-    if isAtEnd {
-      return nil
+    
+    //- (FBEdgeCrossing *) nextNonself
+    var nextNonself : FBEdgeCrossing? {
+        var nextNon : FBEdgeCrossing? = next
+        while nextNon != nil && nextNon!.isSelfCrossing {
+            nextNon = nextNon!.next
+        }
+        return nextNon
     }
-
-    if edge == _intersection.curve1 {
-      return _intersection.curve1RightBezier
-    } else {
-      return _intersection.curve2RightBezier
+    
+    //- (FBEdgeCrossing *) previousNonself
+    var previousNonself : FBEdgeCrossing? {
+        var prevNon : FBEdgeCrossing? = previous
+        while prevNon != nil && prevNon!.isSelfCrossing {
+            prevNon = prevNon!.previous
+        }
+        return prevNon
     }
-  }
-
-  //- (BOOL) isAtStart
-  var isAtStart : Bool {
-    if edge == _intersection.curve1 {
-      return _intersection.isAtStartOfCurve1
-    } else {
-      return _intersection.isAtStartOfCurve2
+    
+    // MARK: Underlying Intersection Access
+    // These properties pass through to the underlying intersection
+    
+    //- (CGFloat) parameter
+    var parameter : Double {
+        // TODO: Is this actually working? Check equality operator here!
+        if edge == _intersection.curve1 {
+            return _intersection.parameter1
+        } else {
+            return _intersection.parameter2
+        }
     }
-  }
-
-  //- (BOOL) isAtEnd
-  var isAtEnd : Bool {
-    if edge == _intersection.curve1 {
-      return _intersection.isAtStopOfCurve1
-    } else {
-      return _intersection.isAtStopOfCurve2
+    
+    //- (NSPoint) location
+    var location : CGPoint {
+        return _intersection.location
     }
-  }
-
+    
+    //- (FBBezierCurve *) curve
+    var curve : FBBezierCurve? {
+        return edge
+    }
+    
+    //- (FBBezierCurve *) leftCurve
+    var leftCurve : FBBezierCurve? {
+        if isAtStart {
+            return nil
+        }
+        
+        if edge == _intersection.curve1 {
+            return _intersection.curve1LeftBezier
+        } else {
+            return _intersection.curve2LeftBezier
+        }
+    }
+    
+    //- (FBBezierCurve *) rightCurve
+    var rightCurve : FBBezierCurve? {
+        if isAtEnd {
+            return nil
+        }
+        
+        if edge == _intersection.curve1 {
+            return _intersection.curve1RightBezier
+        } else {
+            return _intersection.curve2RightBezier
+        }
+    }
+    
+    //- (BOOL) isAtStart
+    var isAtStart : Bool {
+        if edge == _intersection.curve1 {
+            return _intersection.isAtStartOfCurve1
+        } else {
+            return _intersection.isAtStartOfCurve2
+        }
+    }
+    
+    //- (BOOL) isAtEnd
+    var isAtEnd : Bool {
+        if edge == _intersection.curve1 {
+            return _intersection.isAtStopOfCurve1
+        } else {
+            return _intersection.isAtStopOfCurve2
+        }
+    }
+    
 }
