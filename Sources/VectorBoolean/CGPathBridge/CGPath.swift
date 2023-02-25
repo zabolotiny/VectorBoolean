4155446755340797c3f3dad98be09772ee30458b
Edon <edon@valdman.works>
2023-02-25 13:57:50 -0500

    Update CGPath.swift
    Removed MyPathApplier and myPathApply(_:block:), and refactored code of CGPath.apply(_:) to use more recently-added applyWithBlock(_:).

--- Sources/VectorBoolean/CGPathBridge/CGPath.swift
+++ Sources/VectorBoolean/CGPathBridge/CGPath.swift
@@ -8,20 +8,6 @@
 
 import QuartzCore
 
-typealias MyPathApplier = @convention(block) (UnsafePointer<CGPathElement>) -> Void
-// Note: You must declare MyPathApplier as @convention(block), because
-// if you don't, you get "fatal error: can't unsafeBitCast between
-// types of different sizes" at runtime, on Mac OS X at least.
-
-private func myPathApply(_ path: CGPath!, block: MyPathApplier) {
-  let callback: @convention(c) (UnsafeMutableRawPointer, UnsafePointer<CGPathElement>) -> Void = { (info, element) in
-    let block = unsafeBitCast(info, to: MyPathApplier.self)
-    block(element)
-  }
-
-  path.apply(info: unsafeBitCast(block, to: UnsafeMutableRawPointer.self), function: unsafeBitCast(callback, to: CGPathApplierFunction.self))
-}
-
 public enum PathElement {
   case move(to: CGPoint)
   case line(to: CGPoint)
@@ -31,27 +17,29 @@ public enum PathElement {
 }
 
 public extension CGPath {
-
-  func apply(_ fn: (PathElement) -> Void) {
-    myPathApply(self) { element in
-      let points = element.pointee.points
-      switch (element.pointee.type) {
-
-      case CGPathElementType.moveToPoint:
-        fn(.move(to: points[0]))
-
-      case .addLineToPoint:
-        fn(.line(to: points[0]))
-
-      case .addQuadCurveToPoint:
-        fn(.quadCurve(to: points[1], via: points[0]))
-
-      case .addCurveToPoint:
-        fn(.cubicCurve(to: points[2], v1: points[0], v2: points[1]))
-
-      case .closeSubpath:
-        fn(.close)
-      }
+    
+    func apply(_ fn: (PathElement) -> Void) {
+        applyWithBlock { element in
+            let points = element.pointee.points
+            switch (element.pointee.type) {
+                
+            case CGPathElementType.moveToPoint:
+                fn(.move(to: points[0]))
+                
+            case .addLineToPoint:
+                fn(.line(to: points[0]))
+                
+            case .addQuadCurveToPoint:
+                fn(.quadCurve(to: points[1], via: points[0]))
+                
+            case .addCurveToPoint:
+                fn(.cubicCurve(to: points[2], v1: points[0], v2: points[1]))
+                
+            case .closeSubpath:
+                fn(.close)
+            @unknown default:
+                fn(.close)
+            }
+        }
     }
-  }
 }
